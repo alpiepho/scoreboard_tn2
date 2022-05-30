cf40dba3459ea662692340ea1ee3af4c2d4b296a
apiepho <alpiepho@gmail.com>
2022-05-30 03:24:53 -0600

    Built web
    Branch: main
    Commit: d2e920767fab83637adb119b6cf853ad4504e0d3 (dirty)
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6addc7dd22da6896e94424d2237c72b2",
-"index.html": "d9cb0e007a03aa594e02bb817be235c3",
-"/": "d9cb0e007a03aa594e02bb817be235c3",
-"main.dart.js": "918aff44ee290a5b5d84b9b24074aa4f",
+"index.html": "a2040bc13f226a2a1e7e9fbc6d9f830e",
+"/": "a2040bc13f226a2a1e7e9fbc6d9f830e",
+"main.dart.js": "357f3bb18234e55cfc858a5b67d6df02",
 "favicon.png": "e77f5d3b4801779bc0e4be1f4c06c63a",
 "icons/1024.png": "1f684a8e4694e7d3c9ef5528fd64df7f",
 "icons/60.png": "e25184151b617b00196add26237fdaa9",
